deddddc6744e9fa32d982462c20b9364b2597d6f
zikisa <zikisacalesogaxa@gmail.com>
2025-04-13 16:54:46 +0200

    password generator

---
+++ passwordGen.js
@@ -0,0 +1,18 @@
+const num   = [1,2,3,4,5,6,7,8,9,0],
+      sym   = ['.','!','@','#','$','%','^','&','*','(',')','_','+','{','}','<','>','?','|',';','=','-']
+      alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+const generatePass = (len) => {
+    let pass = '';
+
+    do {
+        let ranSym = sym[Math.floor(Math.random() * sym.length)];
+        let ranNum = num[Math.floor(Math.random() * num.length)];
+        let ranAlpha = alpha[Math.floor(Math.random() * alpha.length)];
+        pass+= `${ranSym}${ranNum}${ranAlpha}`;
+    } while (pass.length < len);
+
+    return pass;
+}
+
+console.log(generatePass(20));
